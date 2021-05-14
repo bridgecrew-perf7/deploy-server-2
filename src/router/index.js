d82fe443f1d08ccce907599f2e27ec02c2aa0334
taka <taka@huangguanghongdeMac-mini.local>
2021-05-14 09:38:58 +0800

    update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import { Router } from 'express'
-import i18n from './i18n.js'
+import fs from 'fs'
 const router = Router()
-router.use('/', i18n)
+
+
 
 router.get('/test', (req, res, next)=> {
     console.log('get test')
     res.send('deploy server is fine')
+})
+
+router.post('/deploy', (req, res, next)=> {
+  const b = Buffer.from(req.body.data)
+  const name = req.query.name
+  console.log('p', p, 'name', name)
+  const output = `deploy/${name}.tar.gz`
+  fs.writeFileSync(output, b)
+  res.json({
+    status: 'ok'
+  })
+
 })
 
 export default router
